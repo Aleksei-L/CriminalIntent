ea694ff4189a395b1c453b522d13acf46ca4b5ab
Aleksei Lobanov <lobanov782@gmail.com>
2023-12-08 00:12:59 +0300

    First version

---
+++ app/src/main/java/com/example/criminalintent/activity/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.criminalintent.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.criminalintent.R
+import com.example.criminalintent.fragment.CrimeFragment
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+		if (currentFragment == null) {
+			val fragment = CrimeFragment()
+			supportFragmentManager
+				.beginTransaction()
+				.add(R.id.fragment_container, fragment)
+				.commit()
+		}
+	}
+}
